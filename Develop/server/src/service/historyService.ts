39c439501cae67cb4dedad4f70d234f13def62dc
Fredbo561 <fredbo561@gmail.com>
2025-01-31 22:00:35 -0500

    more work on historyservice

--- Develop/server/src/service/historyService.ts
+++ Develop/server/src/service/historyService.ts
@@ -45,6 +45,17 @@ class HistoryService {
     if (!city) {
       throw new Error('City cannot be blank');
     }
+    const newCity: City = { name: city, id: uuidv4() };
+    
+    return await this.getCities()
+      .then((cities) => {
+        if (cities.find((index) => index.name === city)) {
+          return cities;
+        }
+        return [...cities, newCity];
+      })
+      .then((updatedStates) => this.write(updatedStates))
+      .then(() => newCity)
   }
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   async removeCity(id: string) {
